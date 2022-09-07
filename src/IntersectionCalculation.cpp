5fc63e611fb6c5d576b4312206679bcdff6f9c95
我来人间窜酒局 <granbao@gmail.com>
2022-09-07 22:05:47 +0800

    update

--- src/IntersectionCalculation.cpp
+++ src/IntersectionCalculation.cpp
@@ -2,11 +2,31 @@
 
 bool Intersect::IntersectObbWithLine(const OBB& obb, const Line& line)
 {
+    /// TODO 实现步骤 
+    // 以OBB创建坐标系
+
+    // 将OBB & Line 转换到以OBB创建的坐标系下 
+
+    // 分别计算线的两个方向的射线是否与OBB相交 
+
+    // 只要有一个方向发生相交 则相交 
+
 
     return false;
 }
 
 bool Intersect::IntersectObbWithTriangle(const OBB& obb, const Triangle& triagle)
 {
+    /// TODO 实现步骤 
+    // 以OBB创建坐标系
+
+    // 将OBB & Triangle 转换到以OBB创建的坐标系下 
+
+    // 计算相交 
+
+
     return false;
 }
+
+
+
