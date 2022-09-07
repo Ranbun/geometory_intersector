2329331b9c0a50b6af91d94d1eb8a401e95765c2
我来人间窜酒局 <granbao@gmail.com>
2022-09-07 22:12:14 +0800

    update

--- src/IntersectionCalculation.cpp
+++ src/IntersectionCalculation.cpp
@@ -18,10 +18,6 @@ bool Intersect::IntersectObbWithLine(const OBB& obb, const Line& line)
 bool Intersect::IntersectTriangleWithTriangle(const Triangle& obb, const Triangle& triagle)
 {
     /// TODO 实现步骤 
-    // 以OBB创建坐标系
-
-    // 将OBB & Triangle 转换到以OBB创建的坐标系下 
-
     // 计算相交 
 
 
